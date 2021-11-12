9d47ff14eb5ddd6bda3279ae234a8396d71fa8bf
qtpython <qtkhajacloud@gmail.com>
2021-11-12 18:08:05 +0530

    Added get individual recipe

--- hello_api/app.py
+++ hello_api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from models import recipes
+from http import HTTPStatus
 
 app = Flask(__name__)
 
@@ -11,6 +12,20 @@ def get_all_recipes():
     :return: Json data with all the recipes
     """
     return jsonify(data=recipes)
+
+
+@app.route('/recipes/<int:recipe_id>')
+def get_recipe(recipe_id):
+    """
+    This method will return the recipe if the id is found
+    else returns NOT FOUND
+    :param recipe_id: id of the recipe
+    :return: recipe if found else NOT_FOUND status
+    """
+    recipe_found = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+    if recipe_found:
+        return jsonify(recipe_found)
+    return jsonify(message='recipe not found'), HTTPStatus.NOT_FOUND
 
 
 if __name__ == '__main__':
