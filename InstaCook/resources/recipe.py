380cbdee71d3103fbf48a658d0c4c4db6ac7f7c6
qtdevops <qtdevops@gmail.com>
2021-11-18 17:31:31 +0530

    Added recipe Resource for get specific Recipe and put Recipe

--- InstaCook/resources/recipe.py
+++ InstaCook/resources/recipe.py
@@ -23,7 +23,7 @@ class RecipeListResource(Resource):
     def post(self):
         """
         This method will indicate the post verb
-        :return:
+        :return: recipe data
         """
         data = request.get_json()
         recipe = Recipe(name=data['name'],
@@ -33,3 +33,50 @@ class RecipeListResource(Resource):
                         directions=data['directions'])
         recipe_list.append(recipe)
         return recipe.data, HTTPStatus.CREATED
+
+
+class RecipeResource(Resource):
+    """
+    This class implements the put and get specific recipe implementations
+    """
+
+    def get(self, recipe_id):
+        """
+        This method implements the get request on the specific id
+        :param recipe_id: recipe id to be returned
+        :return: recipe if found else NOT_FOUND status
+        """
+        recipe = self.find_recipe(recipe_id)
+
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+
+        return recipe.data, HTTPStatus.OK
+
+    def put(self, recipe_id):
+        """
+        This method will implement the PUT request
+        :param recipe_id: recipe_id to be updated
+        :return: Updated Recipe data if recipe found else NOT_FOUND status
+        """
+        recipe = self.find_recipe(recipe_id)
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+
+        data = request.get_json()
+        recipe.name = data['name']
+        recipe.directions = data['directions']
+        recipe.num_of_servings = data['num_of_servings']
+        recipe.cook_time = data['cook_time']
+        recipe.description = data['description']
+        return recipe.data, HTTPStatus.OK
+
+    @staticmethod
+    def find_recipe(recipe_id) -> Recipe:
+        """
+        This method finds the recipe with the id provide
+        :param recipe_id: id of the recipe
+        :return: recipe if found None Otherwise
+        """
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+        return recipe
