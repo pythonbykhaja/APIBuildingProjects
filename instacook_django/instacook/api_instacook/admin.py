661f2eafbbe4e2ad4a05294ca3f6d7b2d1c766b0
qtdevops <qtdevops@gmail.com>
2021-12-19 12:59:09 +0530

    Added changes for creating a model using django orm

--- instacook_django/instacook/api_instacook/admin.py
+++ instacook_django/instacook/api_instacook/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from api_instacook.models import Recipe
+
+admin.site.register(Recipe)
--- instacook_django/instacook/api_instacook/models.py
+++ instacook_django/instacook/api_instacook/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Recipe(models.Model):
+    """
+    This class represents the Recipe Model
+    """
+    class Meta:
+        db_table = 'recipe'
+
+    name = models.TextField(null=False)
+    description = models.CharField(max_length=256)
+    number_of_servings = models.IntegerField()
+    cook_time = models.IntegerField()
+    direction = models.CharField(max_length=1000)
+    is_publish = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+
