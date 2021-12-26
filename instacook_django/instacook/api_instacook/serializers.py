02ae39274ab777ff3d42be4e6b6aaebb63c63014
qtdevops <qtdevops@gmail.com>
2021-12-26 12:13:46 +0530

    Added changes to implement Validation

--- instacook_django/instacook/api_instacook/serializers.py
+++ instacook_django/instacook/api_instacook/serializers.py
@@ -18,11 +18,34 @@ class RecipeListSerializer(serializers.ModelSerializer):
             'num_of_servings'
         ]
 
+    def validate_num_of_servings(self, value):
+        """
+        Validates num of servings
+        """
+        if value < 1:
+            raise serializers.ValidationError('Number of servings must be greater than 0.')
+        if value > 50:
+            raise serializers.ValidationError('Number of servings must not be greater than 50. ')
+        return value
+
+    def validate_cook_time(self, value):
+        """
+        This method validates the cook time
+        :param value: value of the Cook time
+        :return: Validation Errors if validation fails
+        """
+        if value < 1:
+            return serializers.ValidationError('Cook time must be greater than 0. ')
+        if value > 300:
+            return serializers.ValidationError('Cook time cannot be greater than 300. ')
+        return value
+
 
 class RecipePublishBaseSerializer(serializers.ModelSerializer):
     """
     This class will help for publishing and un publishing recipes
     """
+
     class Meta:
         model = Recipe
         fields = ['is_publish']
@@ -47,7 +70,6 @@ class RecipeUnPublishSerializer(RecipePublishBaseSerializer):
 
 
 class RecipeDetailSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Recipe
         fields = [
